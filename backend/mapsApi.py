a4e778d26d08d3b156f7754cc69000a22cbfde7e
Sunbakedcow <sunbakedcow@gmail.com>
2019-11-10 01:52:24 -0500

    Add files via upload

---
+++ backend/mapsApi.py
@@ -0,0 +1,16 @@
+import requests
+
+apiKey = open("apiKey.txt", "r").read()
+
+def getLat_Lng(address):
+    url = ('https://maps.googleapis.com/maps/api/geocode/json?address={}&key={}'.format(address.replace(' ','+'), apiKey))
+    try:
+        response = requests.get(url)
+        pythonConvert = response.json()
+        lat = pythonConvert['results'][0]['geometry']['location']['lat']
+        lng = pythonConvert['results'][0]['geometry']['location']['lng']
+    except:
+        lat = 0
+        lng = 0
+    return lat, lng
+    
