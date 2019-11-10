3d4b3021bbba3b97fcb757f8839543536539dea2
Karl <jocelinc60@outlook.com>
2019-11-09 20:45:04 -0500

    added more functions

--- backend/mapsApi.py
+++ backend/mapsApi.py
@@ -2,7 +2,7 @@ import requests
 
 apiKey = open("apiKey.txt", "r").read()
 
-def getLat_Lng(Address):
+def getLat_Lng(address):
     url = ('https://maps.googleapis.com/maps/api/geocode/json?address={}&key={}'.format(address.replace(' ','+'), apiKey))
     try:
         response = requests.get(url)
@@ -14,4 +14,22 @@ def getLat_Lng(Address):
         lng = 0
     return lat, lng
 
+def formalAddress(address):
+    url = ('https://maps.googleapis.com/maps/api/geocode/json?address={}&key={}'.format(address.replace(' ','+'), apiKey))
+    try:
+        repsone = requests.get(url)
+        pythonConvert = response.json()
+        formal = pythonConvert['results'][1]
+    except:
+        formal = ""
+    return formal
 
+def reverseSearch(lat, lng):
+    url = "https://maps.googleapis.com/maps/api/geocode/json?latlng={},{}&key={}".format(lat, lng, apiKey)
+    try:
+        response = requests.get(url)
+        pythonConvert = response.json()
+        address = pythonConvert['results'][1]['formatted_address']
+    except:
+        address = "not working"
+    return address
