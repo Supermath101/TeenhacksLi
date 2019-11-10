da6e12afe3f941dfee657d458997c64f8c90ba5d
Karl <jocelinc60@outlook.com>
2019-11-10 02:09:24 -0500

    added mapsApit

---
+++ backend/mapsApi.py
@@ -0,0 +1,26 @@
+import requests
+
+apiKey = open("apiKey.txt", "r").read()
+
+def getLat_Lng(address):
+    url = ('https://maps.googleapis.com/maps/api/geocode/json?address={}&key={}'.format(address.replace(' ','+'), apiKey))
+    try:
+        response = requests.get(url)
+        pythonConvert = response.json()
+        lat = pythonConvert['results'][0]['geometry']['location']['lat']
+        lng = pythonConvert['results'][0]['geometry']['location']['lng']
+    except:
+        lat = 0
+        lng = 0
+    return lat, lng
+
+
+def formal(lat, lng):
+    url = ('https://maps.googleapis.com/maps/api/geocode/json?latlng={},{}&key={}'.format(lat, lng, apiKey))
+    try:
+        response = requests.get(url)
+        pythonConvert = response.json()
+        address = pythonConvert['results'][1]["formatted_address"]
+    except:
+        address = ""
+    return address
